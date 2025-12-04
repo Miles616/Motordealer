0463d935c48b4d76af95cb6d1ca1fbac56882a1b
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-04 08:14:47 +0000

    replace Space with Motor Khan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           </button>
         </div>
         <div className="left-menu-middle">
-          <div className="logo">SPACE</div>
+          <div className="logo">Motor Khan</div>
         </div>
         <div className="section-nav">
           <a className="section-nav-item" data-index="0">
